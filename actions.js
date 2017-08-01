3715ff979ba138e04332525e28c91fb6543216fe
Roy Feintuch <froyke@gmail.com>
2017-08-01 16:17:17 +0300

    Improved SNS error message

--- actions.js
+++ actions.js
@@ -131,7 +131,7 @@ function sendSNS(rule, entity, ARN) {
     };
     
     sns.publish(params, function (err, data) {
-        if (err) console.error(err, err.stack); // an error occurred
+        if (err) console.error("[sendSNS] Error occured while sending to SNS ", err, err.stack); // an error occurred
         //else console.log(data);           // successful response
     });
 }
